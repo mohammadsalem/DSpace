9e97aec42f2f63b3804d56b8b270f4b3bc1f4b7c
Philip Vissenaekens <philipvissenaekens@atmire.com>
2017-05-02 17:47:28 +0200

    41041: CUA fix

--- dspace/modules/additions/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.0_2017.04.28__CUA_eperson_metadata_migration.sql
+++ dspace/modules/additions/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.0_2017.04.28__CUA_eperson_metadata_migration.sql
@@ -1,34 +1,43 @@
-DROP VIEW IF EXISTS eperson_metadata;
+CREATE OR REPLACE VIEW eperson_metadata AS
 
-CREATE VIEW eperson_metadata AS SELECT
- eperson_id, email, netid,
- mdv_language.text_value AS language, mdv_firstname.text_value AS firstname, mdv_lastname.text_value AS lastname
+  SELECT eperson.eperson_id, email, netid,
+    mdv_language.text_value AS language, mdv_firstname.text_value AS firstname, mdv_lastname.text_value AS lastname
 
- FROM eperson,
- metadatavalue mdv_language,  metadatafieldregistry mdf_language,  metadataschemaregistry mds_language,
- metadatavalue mdv_firstname, metadatafieldregistry mdf_firstname, metadataschemaregistry mds_firstname,
- metadatavalue mdv_lastname,  metadatafieldregistry mdf_lastname,  metadataschemaregistry mds_lastname
+  FROM EPerson eperson
 
-  WHERE mdv_language .resource_id = eperson.eperson_id
-  AND   mdv_firstname.resource_id = eperson.eperson_id
-  AND   mdv_lastname .resource_id = eperson.eperson_id
+    LEFT OUTER JOIN (
+                      SELECT text_value, resource_id
+                      FROM metadatavalue, metadatafieldregistry, metadataschemaregistry
+                      WHERE metadatavalue.metadata_field_id = metadatafieldregistry.metadata_field_id
+                            AND metadatafieldregistry.metadata_schema_id = metadataschemaregistry.metadata_schema_id
 
-  AND   mdf_language .metadata_field_id = mdv_language .metadata_field_id
-  AND   mdf_firstname.metadata_field_id = mdv_firstname.metadata_field_id
-  AND   mdf_lastname .metadata_field_id = mdv_lastname .metadata_field_id
+                            AND metadataschemaregistry.short_id = 'eperson'
+                            AND metadatafieldregistry.element = 'language'
+                            AND metadatafieldregistry.qualifier is null
+                    ) AS mdv_language
+      ON mdv_language.resource_id = eperson.eperson_id
 
-  AND   mds_language .metadata_schema_id = mds_language .metadata_schema_id
-  AND   mds_firstname.metadata_schema_id = mds_firstname.metadata_schema_id
-  AND   mds_lastname .metadata_schema_id = mds_lastname .metadata_schema_id
+    LEFT OUTER JOIN (
+                      SELECT text_value, resource_id
+                      FROM metadatavalue, metadatafieldregistry, metadataschemaregistry
+                      WHERE metadatavalue.metadata_field_id = metadatafieldregistry.metadata_field_id
+                            AND metadatafieldregistry.metadata_schema_id = metadataschemaregistry.metadata_schema_id
 
-  AND   mds_language .short_id = 'eperson'
-  AND   mds_firstname.short_id = 'eperson'
-  AND   mds_lastname .short_id = 'eperson'
+                            AND metadataschemaregistry.short_id = 'eperson'
+                            AND metadatafieldregistry.element = 'firstname'
+                            AND metadatafieldregistry.qualifier is null
+                    ) AS mdv_firstname
+      ON mdv_firstname.resource_id = eperson.eperson_id
 
-  AND   mdf_language .element = 'language'
-  AND   mdf_firstname.element = 'firstname'
-  AND   mdf_lastname .element = 'lastname'
+    LEFT OUTER JOIN (
+                      SELECT text_value, resource_id
+                      FROM metadatavalue, metadatafieldregistry, metadataschemaregistry
+                      WHERE metadatavalue.metadata_field_id = metadatafieldregistry.metadata_field_id
+                            AND metadatafieldregistry.metadata_schema_id = metadataschemaregistry.metadata_schema_id
 
-  AND   mdf_language .qualifier is null
-  AND   mdf_firstname.qualifier is null
-  AND   mdf_lastname .qualifier is null;
+                            AND metadataschemaregistry.short_id = 'eperson'
+                            AND metadatafieldregistry.element = 'lastname'
+                            AND metadatafieldregistry.qualifier is null
+                    ) AS mdv_lastname
+      ON mdv_lastname.resource_id = eperson.eperson_id
+;
