5982be4ebb173ae5b7dd3680636c19d829f50fb2
Alan Orth <alan.orth@gmail.com>
2020-02-05 13:48:55 +0200

    xmlui: Disable browse links in communities and collections
    For some reason the only way to disable these super annoying links
    is to override their browse artifacts and make them print nothing.
    I don't know why our DSpace 5.x instance doesn't show these links,
    as the code there is the same and it has not been overridden.

---
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/browseArtifacts/CommunityBrowse.java
@@ -0,0 +1,56 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.xmlui.aspect.browseArtifacts;
+
+import org.apache.cocoon.ProcessingException;
+import org.dspace.app.xmlui.cocoon.AbstractDSpaceTransformer;
+import org.dspace.app.xmlui.utils.HandleUtil;
+import org.dspace.app.xmlui.wing.Message;
+import org.dspace.app.xmlui.wing.WingException;
+import org.dspace.app.xmlui.wing.element.Body;
+import org.dspace.app.xmlui.wing.element.Division;
+import org.dspace.app.xmlui.wing.element.List;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.browse.BrowseException;
+import org.dspace.browse.BrowseIndex;
+import org.dspace.content.Community;
+import org.dspace.content.DSpaceObject;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Renders the browse links for a community
+ *
+ * @author Kevin Van de Velde (kevin at atmire dot com)
+ * @author Mark Diggory (markd at atmire dot com)
+ * @author Ben Bosman (ben at atmire dot com)
+ */
+public class CommunityBrowse extends AbstractDSpaceTransformer {
+
+    private static final Message T_head_browse =
+        message("xmlui.ArtifactBrowser.CommunityViewer.head_browse");
+
+    private static final Message T_browse_titles =
+        message("xmlui.ArtifactBrowser.CommunityViewer.browse_titles");
+
+    private static final Message T_browse_authors =
+        message("xmlui.ArtifactBrowser.CommunityViewer.browse_authors");
+
+    private static final Message T_browse_dates =
+        message("xmlui.ArtifactBrowser.CommunityViewer.browse_dates");
+
+    @Override
+    public void addBody(Body body) throws SAXException, WingException, SQLException, IOException, AuthorizeException, ProcessingException {
+        // don't print any browse buttons on community pages
+        return;
+    }
+}
