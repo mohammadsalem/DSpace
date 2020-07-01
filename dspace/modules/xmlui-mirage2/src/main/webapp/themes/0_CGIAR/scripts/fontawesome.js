5f202b82a6419e247e12ab41054e14bc6eeda3d7
Alan Orth <alan.orth@gmail.com>
2020-07-01 22:00:18 +0300

    Revert "Revert "xmlui: Use Font Awesome SVG via JS instead of CSS""
    This reverts commit 499af2f741f4aa255bce9a7b3d6722ab7b8205b3.
    
    Atmire CUA now supports Font Awesome 5.

---
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/0_CGIAR/scripts/fontawesome.js
@@ -0,0 +1,13 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faRss, faAt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookSquare, faTwitterSquare, faLinkedin, faMendeley, faGithub, faOrcid } from '@fortawesome/free-brands-svg-icons'
+
+// Add solid icons to our library
+library.add(faRss, faAt, faEnvelope)
+
+// Add brand icons to our library
+library.add(faFacebookSquare, faTwitterSquare, faLinkedin, faMendeley, faGithub, faOrcid)
+
+// Replace any existing <i> tags with <svg> and set up a MutationObserver to
+// continue doing this as the DOM changes.
+dom.watch()
