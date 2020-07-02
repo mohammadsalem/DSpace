dc66251961b279527932aef6beb60fe291309e13
Alan Orth <alan.orth@gmail.com>
2020-07-02 09:28:13 +0300

    xmlui: Add Font Awesome icons used by Atmire CUA

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/0_CGIAR/scripts/fontawesome.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/0_CGIAR/scripts/fontawesome.js
@@ -1,9 +1,9 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faRss, faAt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faRss, faAt, faEnvelope, faChartBar, faTable } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookSquare, faTwitterSquare, faLinkedin, faMendeley, faGithub, faOrcid } from '@fortawesome/free-brands-svg-icons'
 
 // Add solid icons to our library
-library.add(faRss, faAt, faEnvelope)
+library.add(faRss, faAt, faEnvelope, faChartBar, faTable)
 
 // Add brand icons to our library
 library.add(faFacebookSquare, faTwitterSquare, faLinkedin, faMendeley, faGithub, faOrcid)
